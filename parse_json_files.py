d93f0eb3e97ce3d80b026c883c41cd5cd331ada7
Joas Hoogendijk <joas.hoogendijk@gmail.com>
2018-09-13 20:53:24 +0200

    Initial commit Implemented functions for parsing except the parsing itself

---
+++ parse_json_files.py
@@ -0,0 +1,41 @@
+import os
+from re import match
+import json
+
+dialogues = []
+
+
+def main():
+    iterate_over_folders(read_json_files)
+    write_dialogue_list_to_file()
+
+
+def write_to_dialogue_list(dialogue: str):
+    dialogues.append(dialogue)
+
+
+def write_dialogue_list_to_file():
+    dialogues_as_text = json.dumps(dialogues)
+    with open("", "rw") as f:
+        f.write(dialogues_as_text)
+        f.close()
+
+
+def read_json_files(write_function: function, dialogue_id: str):
+    pass
+
+
+def directory_name_is_identifier(name):
+    return match(r"voip-.*", name) is not None
+
+
+def iterate_over_folders(node_function: function, parent_dir = ""):
+    for name in os.listdir("."):
+        if directory_name_is_identifier(parent_dir):
+            node_function(write_to_dialogue_list, parent_dir)
+        elif os.path.isdir(name):
+            iterate_over_folders(read_json_files, name)
+
+
+main()
+print("Dialogues file created")
