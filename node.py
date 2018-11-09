2aa7aaf708683b4b41d8df6927d9d1956043d857
augustaugust <augustedstrom@hotmail.com>
2018-11-09 21:44:56 +0100

    Integrated new part2 with part4 and updated keywords and lexicon with 'any'

--- node.py
+++ node.py
@@ -12,30 +12,30 @@ class Node:
     @staticmethod
     def remove_brackets(word):
         return_word = word
-        print("in remove brackets ", return_word)
+        #print("in remove brackets ", return_word)
         if return_word[0] == "(":
             return_word = return_word[1::]
         if return_word[(len(return_word) - 1)] == ")":
             return_word = return_word[:-1]
-        print(return_word, " is returned")
+        #print(return_word, " is returned")
         return return_word
 
     def add_child(self, child):
         self.children.append(child)
 
     def is_compatible_with(self, right):
-        print("*******************")
-        print("left  text:", self.text)
-        print("      types:", self.types)
-        print("right text", right.text)
-        print("      types:", right.types)
-        print("*******************")
+        #print("*******************")
+        #print("left  text:", self.text)
+        #print("      types:", self.types)
+        #print("right text", right.text)
+        #print("      types:", right.types)
+        #print("*******************")
         for left_type in self.types:
             for right_type in right.types:
                 right__type_split = right_type.rsplit("\\", 1)
                 if len(right__type_split) == 2:
                     expected_left_type = right__type_split[0]
-                    print("expected: " + expected_left_type + ", actual: " + left_type)
+                    #print("expected: " + expected_left_type + ", actual: " + left_type)
                     if left_type == self.remove_brackets(expected_left_type):
                         self.type = left_type
                         right.type = right_type
@@ -46,7 +46,7 @@ class Node:
                 left__type_split = left_type.rsplit("/", 1)
                 if len(left__type_split) == 2:
                     expected_right_type = left__type_split[1]
-                    print("expected: " + expected_right_type + ", actual: " + right_type)
+                    #print("expected: " + expected_right_type + ", actual: " + right_type)
                     if right_type == self.remove_brackets(expected_right_type):
                         self.type = left_type
                         right.type = right_type
